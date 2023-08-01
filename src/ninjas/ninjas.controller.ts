189b2bea4cb58922d24af4e357aff741018fe676
John Menard Cerezo <johnmenardccerezo@gmail.com>
2023-08-01 11:14:29 +0800

    MOD: refactor codes in controller and service

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -22,32 +22,39 @@ import { BeltGuard } from 'src/ninjas/belt/belt.guard';
 export class NinjasController {
   constructor(private readonly ninjasService: NinjasService) {}
 
-  @Get()
-  getNinjas(@Query('weapon') weapon: 'stars' | 'nunchucks') {
-    return this.ninjasService.getNinjas(weapon);
-  }
-
-  @Get(':id')
-  getOneNinja(@Param('id', ParseIntPipe) id: number) {
-    try {
-      return this.ninjasService.getNinja(id);
-    } catch {
-      throw new NotFoundException('ninja not found');
-    }
-  }
-
+  // * CREATE
   @Post()
   createNinja(@Body(new ValidationPipe()) createNinjaDto: CreateNinjaDto) {
     return this.ninjasService.createNinja(createNinjaDto);
   }
 
-  @Put(':id')
-  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
-    return this.ninjasService.updateNinja(+id, updateNinjaDto);
+  // * READ
+  @Get()
+  getAllNinjas(@Query('weapon') weapon: 'stars' | 'nunchucks') {
+    return this.ninjasService.getAllNinjas(weapon);
   }
 
+  @Get(':id')
+  getNinjaById(@Param('id', ParseIntPipe) id: number) {
+    try {
+      return this.ninjasService.getNinjaById(id);
+    } catch {
+      throw new NotFoundException('ninja not found');
+    }
+  }
+
+  // * UPDATE
+  @Put(':id')
+  updateNinja(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateNinjaDto: UpdateNinjaDto,
+  ) {
+    return this.ninjasService.updateNinja(id, updateNinjaDto);
+  }
+
+  // * DELETE
   @Delete(':id')
-  removeNinja(@Param('id') id: string) {
-    return this.ninjasService.removeNinja(+id);
+  removeNinja(@Param('id', ParseIntPipe) id: number) {
+    return this.ninjasService.removeNinja(id);
   }
 }
--- src/ninjas/ninjas.service.ts
+++ src/ninjas/ninjas.service.ts
@@ -9,7 +9,20 @@ export class NinjasService {
     { id: 1, name: 'ninja B', weapon: 'nunchucks' },
   ];
 
-  getNinjas(weapon?: 'stars' | 'nunchucks') {
+  // * CREATE
+  createNinja(createNinjaDto: CreateNinjaDto) {
+    const newNinja = {
+      id: Date.now(),
+      ...createNinjaDto,
+    };
+
+    this.ninjas.push(newNinja);
+
+    return newNinja;
+  }
+
+  // * READ
+  getAllNinjas(weapon?: 'stars' | 'nunchucks') {
     if (weapon) {
       return this.ninjas.filter((ninja) => ninja.weapon === weapon);
     }
@@ -17,7 +30,7 @@ export class NinjasService {
     return this.ninjas;
   }
 
-  getNinja(id: number) {
+  getNinjaById(id: number) {
     const ninja = this.ninjas.find((ninja) => ninja.id === id);
 
     if (!ninja) {
@@ -27,17 +40,7 @@ export class NinjasService {
     return ninja;
   }
 
-  createNinja(createNinjaDto: CreateNinjaDto) {
-    const newNinja = {
-      ...createNinjaDto,
-      id: Date.now(),
-    };
-
-    this.ninjas.push(newNinja);
-
-    return newNinja;
-  }
-
+  //  * UPDATE
   updateNinja(id: number, updateNinjaDto: UpdateNinjaDto) {
     this.ninjas = this.ninjas.map((ninja) => {
       if (ninja.id === id) {
@@ -47,11 +50,12 @@ export class NinjasService {
       return ninja;
     });
 
-    return this.getNinja(id);
+    return this.getNinjaById(id);
   }
 
+  // * DELETE
   removeNinja(id: number) {
-    const toBeRemoved = this.getNinja(id);
+    const toBeRemoved = this.getNinjaById(id);
 
     this.ninjas = this.ninjas.filter((ninja) => ninja.id !== id);
 
